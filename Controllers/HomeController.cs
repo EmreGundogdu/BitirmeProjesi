a30e67134a982ac242685ac8f6aebeeb9b27b666
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-10-26 19:52:58 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ETicaret.WebUI.Models;
+using ETicaret.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicaret.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>()
+            {
+                new Product {Name="Iphone 7",Price=3000,Description="iyi telefon",IsApproved=false},
+                new Product {Name="Iphone 8",Price=4000,Description="çok iyi telefon",IsApproved=true},
+                new Product {Name="Iphone X",Price=5000,Description="çok iyi telefon",IsApproved=true},
+                new Product {Name="Iphone 11",Price=7000,Description="çok iyi telefon"},
+            };
+
+            var productViewModel = new ProductViewModel()
+            {
+                Products = products
+            };
+
+            return View(productViewModel);
+        }
+
+        // localhost:5000/home/about
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View("MyView");
+        }
+    }
+}
---
+++ ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ETicaret.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicaret.WebUI.ViewModels
+{
+    public class ProductViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
